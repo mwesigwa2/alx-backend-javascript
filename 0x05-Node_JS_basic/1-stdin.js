caeca16446137037cbeb97e7d8c211352c6a5f37
Timothy Kibirige <tmwesigwa2@gmail.com>
2024-03-01 08:41:45 +0000

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,21 +1,13 @@
-if (require.main === module) {
-  process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-  process.stdin.on('data', (data) => {
-    const input = data.toString().trim(); // Trim to remove leading/trailing whitespaces
+process.stdin.on('readable', () => {
+    const input = process.stdin.read();
 
     if (input) {
-      process.stdout.write(`Your name is: ${input}\n`);
-    } else {
-      process.stdout.write('This important software is now closing.\n');
+      process.stdout.write(`Your name is: ${input}`);
     }
-
-    // Check if input is coming from a terminal (TTY)
-    if (!process.stdin.isTTY) {
-      process.stdout.write('This important software is now closing.\n');
-      process.exit();
-    }
-
-    process.stdin.pause(); // Close the standard input stream
   });
-}
+
+process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+});
